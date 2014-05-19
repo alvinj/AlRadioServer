ff47d0fa8b82daa8b0c91c435acb719432c76b84
Alvin Alexander <devdaily@gmail.com>
2014-05-19 10:21:09 -0600

    a little more non-working code

--- app/controllers/Radio.scala
+++ app/controllers/Radio.scala
@@ -2,12 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import org.apache.commons.exec._
-import java.io.File
 import utils._
 
 object Radio extends Controller {
   
+    //
+    // TODO move this to the Global object?
+    //
     val host = "localhost"
     val port = 5150
 
@@ -17,53 +18,17 @@ object Radio extends Controller {
         Ok(views.html.index("Your new application is ready."))
     }
 
-    private def startVlcServer {
+    /**
+     * Start a stream, like "wgn.pls" or "104_3.pls".
+     */
+    private def startVlcStreamServer(plsFile: String) {
 
         //
         // TODO working here ...
         //
         val cmd = VlcUtils.buildVlcStreamServerCommand("localhost", 5150, "mac.pls")
-        val watchDog = new ExecuteWatchdog(ExecuteWatchdog.INFINITE_TIMEOUT)
-    
-        // result handler for executing the process in a async way
-        val resultHandler = new DefaultExecuteResultHandler
-    
-        // using stdout for the output/error stream
-        val streamHandler = new PumpStreamHandler
-    
-        //This is used to end the process when the JVM exits
-        val processDestroyer = new ShutdownHookProcessDestroyer
-    
-        //Our main command executor
-        val executor = new DefaultExecutor
-    
-        //Setting the properties
-        executor.setStreamHandler(streamHandler)
-        executor.setWatchdog(watchDog)
-    
-        //Setting the working directory
-        //Use of recursion along with the ls makes this a long running process
-        // executor.setWorkingDirectory(new File("/Users/Al/Projects/Scala/Tests/ApacheExec"))
-        executor.setProcessDestroyer(processDestroyer)
-    
-        //Executing the command
-        executor.execute(cmd, resultHandler)
-        println("*** immediately after 'execute' ***")
-    
-        // execution stops with this statement (until the command returns)
-        resultHandler.waitFor
-        println("*** immediately after 'waitFor' ***")
-    
-        val exitValue = resultHandler.getExitValue
-        println(exitValue)
-    
-        if (executor.isFailure(exitValue)) {
-          System.out.println("Execution failed")
-        } else {
-          System.out.println("Execution Successful")
-        }
-
     }
+    
     
 
 }
--- app/utils/VlcUtils.scala
+++ app/utils/VlcUtils.scala
@@ -1,6 +1,11 @@
 package utils
 
 import org.apache.commons.exec.CommandLine
+import java.net.Socket
+import java.io.PrintWriter
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import org.apache.commons.exec._
 
 object VlcUtils {
 
@@ -16,6 +21,47 @@ object VlcUtils {
         }
     }
     
+    def sendServerShutdownCommand(host: String, port: Int) {
+        writeVlcCommandToSocket(host, port, "shutdown")
+    }
+
+    def increaseVolume(host: String, port: Int) {
+        writeVlcCommandToSocket(host, port, "volup")
+    }
+
+    def increaseVolume(host: String, port: Int, amount: Int) {
+        writeVlcCommandToSocket(host, port, s"volume $amount")
+    }
+
+    def decreaseVolume(host: String, port: Int) {
+        writeVlcCommandToSocket(host, port, "voldown")
+    }
+
+    def decreaseVolume(host: String, port: Int, amount: Int) {
+        writeVlcCommandToSocket(host, port, s"volume $amount")
+    }
+
+    /**
+     * Amount can be "+10", "-10", etc.
+     * TODO I think the units value is "seconds".
+     */
+    def seek(host: String, port: Int, amount: String) {
+        writeVlcCommandToSocket(host, port, s"seek $amount")
+    }
+
+    /**
+     * Sends the given command to the port the VLC server is running on.
+     * Valid commands are "shutdown", "pause", "play", "seek +10", "voldown",
+     * "volup", "volume +10".
+     */
+    private def writeVlcCommandToSocket(host: String, port: Int, command: String) {
+        val socket = new Socket(host, port)
+        val out = new PrintWriter(socket.getOutputStream, true)
+        val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+        out.println(command)
+        socket.close
+    }
+        
     private def buildVlcServerCommandMac(fileOrStream: String, host: String, port: Int) = {
         val cmd = new CommandLine(getVlcServerCommand)
         cmd.addArgument(fileOrStream)
@@ -55,4 +101,44 @@ object VlcUtils {
         runningOnMac
     }
 
+    def executeCommand(cmd: CommandLine) {
+        val watchDog = new ExecuteWatchdog(ExecuteWatchdog.INFINITE_TIMEOUT)
+    
+        // result handler - exec the process in an async way
+        val resultHandler = new DefaultExecuteResultHandler
+    
+        // use stdout for the output/error stream
+        val streamHandler = new PumpStreamHandler
+    
+        // used to end the process when the jvm exits
+        val processDestroyer = new ShutdownHookProcessDestroyer
+    
+        // main command executor
+        val executor = new DefaultExecutor
+    
+        // properties
+        executor.setStreamHandler(streamHandler)
+        executor.setWatchdog(watchDog)
+        executor.setProcessDestroyer(processDestroyer)
+    
+        // exec the command (async)
+        executor.execute(cmd, resultHandler)
+        
+        // TODO i need to understand the code below better
+    
+//        // execution stops with this statement (until the command returns)
+//        resultHandler.waitFor
+//        println("*** immediately after 'waitFor' ***")
+//    
+//        val exitValue = resultHandler.getExitValue
+//        println(exitValue)
+//    
+//        if (executor.isFailure(exitValue)) {
+//          System.out.println("Execution failed")
+//        } else {
+//          System.out.println("Execution Successful")
+//        }
+
+    }
+    
 }
